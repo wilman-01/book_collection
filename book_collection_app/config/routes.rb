66afb0d8e2043db85b15803ccd5743dce3625203
wilman-01 <wilmangabriel2@gmail.com>
2020-09-17 18:45:39 -0500

    initial commit

---
+++ book_collection_app/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  root 'books#index'
+
+  #resources :books
+
+  resources :books do
+    member do
+      get :delete
+    end  
+  end
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
